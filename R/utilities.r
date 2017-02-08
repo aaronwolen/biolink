e1de1861c667a3c687f482efce7021543a5feab5
Aaron Wolen <aaron@wolen.com>
2017-02-08 12:38:36 -0500

    Vectorize check_id()

--- R/utilities.r
+++ R/utilities.r
@@ -8,9 +8,17 @@ check_id <- function(id, type) {
   pattern <- switch(type,
     GO = "GO:[0-9]{7}"
   )
-  if (grepl(pattern, id)) {
+  valid <- grepl(pattern, id)
+  if (all(valid)) {
     return(id)
   } else {
-    stop(id, " is not a valid ", type, " identifier", call. = FALSE)
+    invalid <- id[!valid]
+    n <- length(invalid)
+    if (n == 1) {
+      stop(invalid, " is not a valid ", type, " identifier", call. = FALSE)
+    } else {
+      stop(n, " of the supplied ", type, " idenfitiers are invalid including:\n",
+           paste0(" -", invalid, "\n")[seq_len(min(10, n))], call. = FALSE)
+    }
   }
 }
