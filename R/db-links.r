26682ad7b17aa1b527c5c44580fcbcf13a48f802
Aaron Wolen <aaron@wolen.com>
2017-02-08 13:28:29 -0500

    Combine documentation for GO functions

--- R/db-links.r
+++ R/db-links.r
@@ -1,20 +1,26 @@
 #' Gene Ontology Link
 #'
-#' Construct hypertext link to the Gene Ontology (GO) database.
+#' Construct a URL or hypertext link to the Gene Ontology (GO) database.
 #'
 #' @inheritParams build_link
+#' @param id gene ontology ID (e.g., `"GO:0005539"`)
 #' @param text displayed text, defaults to the supplied `id`
 #'
 #' @references [Gene Ontology Consortium](http://amigo.geneontology.org)
-#' @export
+#'
 #' @examples
 #' go_link("GO:0005539", format = "html")
+#' @name go
+NULL
+
+#' @export
+#' @rdname go
 go_link <- function(id, text = id, title = NULL, format = "html") {
   build_link(go_url(id), text = id, format = format)
 }
 
 #' @export
-#' @describeIn go_link construct bare URL ponting to GO database
+#' @rdname go
 go_url <- function(id) {
   paste0("http://amigo.geneontology.org/amigo/term/", check_id(id, "GO"))
 }
