ce82eee0b09a3333e887a8e8b9ec9de6d7bddc53
Aaron Wolen <aaron@wolen.com>
2017-02-10 14:32:53 -0500

    Add tests for entrez tag retrieval

--- tests/testthat/test-entrez-links.r
+++ tests/testthat/test-entrez-links.r
@@ -52,3 +52,18 @@ test_that("Entrez html link + text + title", {
   link <- entrez_link(id, text, title, format = "html")
   expect_match(link, ref, fixed = TRUE)
 })
+
+
+context("NCBI Entrez tag substitution")
+
+ids         <- c("101954267", "105369219")
+symbols     <- c("RNVU1-15", "LOC105369219")
+location    <- c("1q21.1", "20p13")
+description <- c("RNA, variant U1 small nuclear 15",
+                 "uncharacterized LOC105369219")
+
+test_that("Entrez tag retrieval", {
+  expect_equal(entrez_query(ids, "symbol"),      symbols)
+  expect_equal(entrez_query(ids, "location"),    location)
+  expect_equal(entrez_query(ids, "description"), description)
+})
