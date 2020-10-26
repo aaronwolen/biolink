c850302da09d5e28a3c437668ca2c3770254b7b9
Aaron Wolen <aaron@wolen.com>
2020-10-26 08:09:55 -0500

    Avoid API limit in CI tests

--- tests/testthat/helper-functions.r
+++ tests/testthat/helper-functions.r
@@ -9,3 +9,8 @@ check_url_status <- function(url, n = 3) {
   }
   code
 }
+
+# avoid entrez's api rate limit of 3/sec by sleeping when no API key is found
+sleep_if_keyless <- function() {
+  if (!nzchar(Sys.getenv("ENTREZ_KEY"))) Sys.sleep(1)
+}
--- tests/testthat/test-tags-pubmed.r
+++ tests/testthat/test-tags-pubmed.r
@@ -4,15 +4,20 @@ context("PubMed tag substitution")
 ids <- c("20345488", "20600793")
 years <- c("2010", "2010")
 journals <- c("Cellular microbiology", "Microbial pathogenesis")
-titles <- c("The Anaplasma phagocytophilum-occupied vacuole selectively recruits Rab-GTPases that are predominantly associated with recycling endosomes.", 
+titles <- c("The Anaplasma phagocytophilum-occupied vacuole selectively recruits Rab-GTPases that are predominantly associated with recycling endosomes.",
     "Anaplasma phagocytophilum APH_0032 is expressed late during infection and localizes to the pathogen-occupied vacuolar membrane.")
 # nolint end
 test_that("PubMed tag retrieval", {
   skip_on_cran()
   expect_equal(query_pubmed(ids, "year"), years)
+  sleep_if_keyless()
+
   expect_equal(query_pubmed(ids, "journal"), journals)
+  sleep_if_keyless()
+
   expect_equal(query_pubmed(ids, "title"), titles)
-;})
+  sleep_if_keyless()
+})
 
 test_that("PubMed tag substitution", {
   skip_on_cran()
@@ -22,12 +27,14 @@ test_that("PubMed tag substitution", {
   ref  <- sprintf(hl, url, years[1])
   link <- link_pubmed(id, "<year>", format = "html")
   expect_equal(link, ref)
+  sleep_if_keyless()
 
   ref  <- sprintf(hl, url, journals[1])
   link <- link_pubmed(id, "<journal>", format = "html")
   expect_equal(link, ref)
+  sleep_if_keyless()
 
   ref  <- sprintf(hl, url, titles[1])
   link <- link_pubmed(id, "<title>", format = "html")
   expect_equal(link, ref)
-;})
+})
